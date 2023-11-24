04a135489375fc1c82c3771ca2e8a642715d63b7
John Rowarth <johnrowarth10@gmail.com>
2023-11-24 13:04:31 +0530

    hero responsive design changes

--- components/hero.tsx
+++ components/hero.tsx
@@ -25,17 +25,17 @@ const Hero: React.FC<HeroProps> = ({ data }) => {
         <Image
           src={Stockholm}
           alt="swedish archipelago"
-          className="rounded-md max-w-sm  md:max-w-md lg:mr-12 lg:max-w-md xl:max-w-lg"
+          className="rounded-md max-w-xs sm:max-w-sm  md:max-w-md lg:mr-12 lg:max-w-md xl:max-w-lg"
         />
         <div>
-          <h1 className="text-5xl mb-4 sm:text-6xl md:text-7xl   xl:text-8xl  ">
+          <h1 className=" text-6xl text-center  mb-4 sm:text-6xl sm:text-left md:text-7xl   xl:text-8xl  ">
             Museum&nbsp;
-            <span className="hidden lg:inline">
+            <span className=" inline sm:hidden md:hidden lg:inline">
               <br />
             </span>
             Caliber Art
           </h1>
-          <h2 className="text-4xl mb-9 text-center md:text-5xl lg:text-left    ">
+          <h2 className="text-3xl mb-9 text-center md:text-5xl lg:text-left    ">
             Original Watercolour <br /> Paintings & Commissions
           </h2>
           <div className="flex  justify-center gap-4 sm: mb-8 lg:mb-0   ">
@@ -43,14 +43,14 @@ const Hero: React.FC<HeroProps> = ({ data }) => {
               <Link
                 key={route.href}
                 href={route.href}
-                className="text-2xl font-medium  px-4 py-2 rounded-md border border-black  bg-247BA0 bg-black text-white disabled:cursor-not-allowed hover:bg-white transition  ease-in-out  hover:text-black"
+                className=" text-lg px-2 py-1 rounded-md border border-black  bg-247BA0 bg-black text-white disabled:cursor-not-allowed hover:bg-white transition  ease-in-out  hover:text-black md:text-2xl md:px-4 md:py-2"
               >
                 Shop now
               </Link>
             ))}
             <Link
               href="/commissionWork"
-              className="text-2xl font-medium  px-4 py-2 rounded-md border border-black  bg-247BA0 bg-white  disabled:cursor-not-allowed hover:bg-black transition  ease-in-out  hover:text-white "
+              className=" text-lg  px-2 py-1 rounded-md border border-black  bg-247BA0 bg-white  disabled:cursor-not-allowed hover:bg-black transition  ease-in-out  hover:text-white md:text-2xl md:px-4 md:py-2"
             >
               Commissions
             </Link>
