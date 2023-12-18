66a86809bfae8f6548d61ab51e07ed467e7d97ef
John Rowarth <johnrowarth10@gmail.com>
2023-12-18 08:58:05 +0000

    uncommented pages

--- app/(routes)/commissionWork/page.tsx
+++ app/(routes)/commissionWork/page.tsx
@@ -1,26 +1,26 @@
-// import React from "react";
-// import CommissionForm from "@/components/commissionForm";
-// import Container from "@/components/ui/container";
-// import ArtWall from "@/components/artWall";
+import React from "react";
+import CommissionForm from "@/components/commissionForm";
+import Container from "@/components/ui/container";
+import ArtWall from "@/components/artWall";
 
-// const CommissionWorkPage: React.FC = () => {
-//   return (
-//     <Container>
-//       <div className="pb-12 pt-8 w-3/4 mx-auto text-center ">
-//         <h2 className="text-5xl py-12  ">Commissioned Artwork Inquiry</h2>
-//         <p className="text-lg md:text-xl ">
-//           Thank you for considering a custom artwork! Please fill out the form
-//           below with your contact details. We&apos;ll get in touch within 2 days
-//           to discuss your preferences and the ideas you have in mind. We&apos;re
-//           excited to bring your vision to life!
-//         </p>
-//       </div>
-//       <ArtWall />
-//       <div className="flex gap-6 justify-center items-center">
-//         <CommissionForm />
-//       </div>
-//     </Container>
-//   );
-// };
+const CommissionWorkPage: React.FC = () => {
+  return (
+    <Container>
+      <div className="pb-12 pt-8 w-3/4 mx-auto text-center ">
+        <h2 className="text-5xl py-12  ">Commissioned Artwork Inquiry</h2>
+        <p className="text-lg md:text-xl ">
+          Thank you for considering a custom artwork! Please fill out the form
+          below with your contact details. We&apos;ll get in touch within 2 days
+          to discuss your preferences and the ideas you have in mind. We&apos;re
+          excited to bring your vision to life!
+        </p>
+      </div>
+      <ArtWall />
+      <div className="flex gap-6 justify-center items-center">
+        <CommissionForm />
+      </div>
+    </Container>
+  );
+};
 
-// export default CommissionWorkPage;
+export default CommissionWorkPage;
