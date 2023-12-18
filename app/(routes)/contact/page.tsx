2b20374c248474b5cc92ecbdedcd6d75d6bc0187
John Rowarth <johnrowarth10@gmail.com>
2023-12-18 08:54:08 +0000

    took out commission/contact and cart pages

--- app/(routes)/contact/page.tsx
+++ app/(routes)/contact/page.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import ContactForm from "@/components/contactForm";
-import Container from "@/components/ui/container";
+// import React from "react";
+// import ContactForm from "@/components/contactForm";
+// import Container from "@/components/ui/container";
 
-const ContactPage: React.FC = () => {
-  return (
-    <Container>
-      <div className="flex gap-6 justify-center items-center">
-        <ContactForm />
-      </div>
-    </Container>
-  );
-};
+// const ContactPage: React.FC = () => {
+//   return (
+//     <Container>
+//       <div className="flex gap-6 justify-center items-center">
+//         <ContactForm />
+//       </div>
+//     </Container>
+//   );
+// };
 
-export default ContactPage;
+// export default ContactPage;
