5ee83387e64f9f5f62b9d173fca9845e1a6303f0
John Rowarth <johnrowarth10@gmail.com>
2023-12-18 11:14:12 +0000

    Moved the browse component to the home page due to rendering issues

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,22 +1,52 @@
 import Container from "@/components/ui/container";
-
 import Intro from "../../components/intro";
-
 import Commission from "../../components/commission";
-
 import Review from "@/components/reviews";
 import Hero from "@/components/hero";
+// import Browse from "../../components/browse";
 
-import Browse from "../../components/browse";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+import Link from "next/link";
 
-const HomePage: React.FC = () => {
+export const revalidate = 0;
+
+const HomePage: React.FC = async () => {
+  const products = await getProducts({ isFeatured: true });
+
+  const limitedProducts = products.slice(0, 4);
   return (
     <>
       <div className="bg-gray-100 ">
         <Hero />
       </div>
       <Container>
-        <Browse />
+        {/* <Browse /> */}
+        <div className="flex items-center justify-center my-16 pt-8 pb-20 border-b-2 border-gray-300 ">
+          <div className="w-full rounded-md">
+            <h2 className=" flex justify-center text-5xl  pb-4   ">
+              New Arrivals
+            </h2>
+            <h3 className=" flex justify-center  pb-8 text-xl md:text-2xl ">
+              See what has just been added
+            </h3>
+            <ProductList
+              title=""
+              items={limitedProducts}
+              cardClassName=" "
+              listClassName="px-12"
+            />
+
+            <div className="flex justify-center pt-12 ">
+              <Link
+                href="https://vivid-store.vercel.app/category/c5c2ae9e-963c-4de7-af3e-f240d69f7643"
+                className="text-2xl font-medium  px-4 py-2 rounded-md border border-black  bg-247BA0 bg-black text-white disabled:cursor-not-allowed hover:bg-white transition  ease-in-out  hover:text-black  "
+              >
+                View more
+              </Link>
+            </div>
+          </div>
+        </div>
         <Commission />
         <Intro />
 
--- components/browse.tsx
+++ components/browse.tsx
@@ -1,38 +1,38 @@
-import getProducts from "@/actions/get-products";
-import ProductList from "@/components/product-list";
-import Link from "next/link";
+// import getProducts from "@/actions/get-products";
+// import ProductList from "@/components/product-list";
+// import Link from "next/link";
 
-export const revalidate = 0;
+// export const revalidate = 0;
 
-const Browse: React.FC = async () => {
-  const products = await getProducts({ isFeatured: true });
+// const Browse: React.FC = async () => {
+//   const products = await getProducts({ isFeatured: true });
 
-  const limitedProducts = products.slice(0, 4);
-  return (
-    <div className="flex items-center justify-center my-16 pt-8 pb-20 border-b-2 border-gray-300 ">
-      <div className="w-full rounded-md">
-        <h2 className=" flex justify-center text-5xl  pb-4   ">New Arrivals</h2>
-        <h3 className=" flex justify-center  pb-8 text-xl md:text-2xl ">
-          See what has just been added
-        </h3>
-        <ProductList
-          title=""
-          items={limitedProducts}
-          cardClassName=" "
-          listClassName="px-12"
-        />
+//   const limitedProducts = products.slice(0, 4);
+//   return (
+//     <div className="flex items-center justify-center my-16 pt-8 pb-20 border-b-2 border-gray-300 ">
+//       <div className="w-full rounded-md">
+//         <h2 className=" flex justify-center text-5xl  pb-4   ">New Arrivals</h2>
+//         <h3 className=" flex justify-center  pb-8 text-xl md:text-2xl ">
+//           See what has just been added
+//         </h3>
+//         <ProductList
+//           title=""
+//           items={limitedProducts}
+//           cardClassName=" "
+//           listClassName="px-12"
+//         />
 
-        <div className="flex justify-center pt-12 ">
-          <Link
-            href="https://vivid-store.vercel.app/category/c5c2ae9e-963c-4de7-af3e-f240d69f7643"
-            className="text-2xl font-medium  px-4 py-2 rounded-md border border-black  bg-247BA0 bg-black text-white disabled:cursor-not-allowed hover:bg-white transition  ease-in-out  hover:text-black  "
-          >
-            View more
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
-};
+//         <div className="flex justify-center pt-12 ">
+//           <Link
+//             href="https://vivid-store.vercel.app/category/5099b8f3-1dce-47e8-aa30-d9695dd78ebc"
+//             className="text-2xl font-medium  px-4 py-2 rounded-md border border-black  bg-247BA0 bg-black text-white disabled:cursor-not-allowed hover:bg-white transition  ease-in-out  hover:text-black  "
+//           >
+//             View more
+//           </Link>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Browse;
+// export default Browse;
